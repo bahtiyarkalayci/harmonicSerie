4deb8d2a4bd4b7a3897d0c3cab2a9d8ad3885462
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-28 13:28:06 +0300

    https://www.patika.dev/tr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double arr[]={1.0,2.0,3.0,4.0,5.0};
+
+
+double division=0.0;
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+            division+=1/arr[i];
+            double average=arr.length/division;
+
+        }
+        double average=arr.length/division;
+        System.out.println("harmonic series average="+average);
+
     }
 }
